a226ec3b70cb3b637893bfe9a03efd3f9a4d0ef2
MrBElga <43758059+MrBElga@users.noreply.github.com>
2022-10-31 21:55:30 -0300

    attt

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -2,18 +2,18 @@ function NoticiasDAO(connection){
 	this._connection = connection;
 }
 
-NoticiasDAO.prototype.getNoticias = (callback)=>{
+NoticiasDAO.prototype.getNoticias = function(callback){
 	this._connection.query('select * from noticias', callback);
 }
 
-NoticiasDAO.prototype.getNoticia = (callback)=>{
+NoticiasDAO.prototype.getNoticia = function(callback){
 	this._connection.query('select * from noticias where id_noticia = 2', callback);
 }
 
-NoticiasDAO.prototype.salvarNoticia = (noticia, callback)=>{
+NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
 	this._connection.query('insert into noticias set ? ', noticia, callback)
 }
 
-module.exports = ()=>{
+module.exports = function(){
 	return NoticiasDAO;
 }
